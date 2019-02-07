250c912243bf89e80e82ab50fbea16bd04de7834
Alvaro <alvarorgt@gmail.com>
2019-02-06 22:36:48 -0500

    Se agrega GetScreenShot

--- leerarchivos/src/main/java/QVision/leerarchivos/Principal.java
+++ leerarchivos/src/main/java/QVision/leerarchivos/Principal.java
@@ -2,12 +2,16 @@ package QVision.leerarchivos;
 
 import java.io.BufferedReader;
 import java.io.File;
+import org.apache.commons.io.FileUtils;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,6 +24,7 @@ public class Principal {
 		WebDriver driver = new ChromeDriver();
 		File data = new File("Users.txt");
 		FileReader fr;
+		
 		//driver.get("http://sahitest.com/demo/training/login.htm");
 		try {
 			fr = new FileReader(data);
@@ -37,9 +42,16 @@ public class Principal {
 					.findElement(By.xpath("/html/body/center/div/form/table/tbody/tr[3]/td[2]/input"));
 			BtnLogin.click();
 			WebElement lblerror = driver.findElement(By.xpath("//*[@id=\"errorMessage\"]"));
+			TakesScreenshot scrShot =((TakesScreenshot)driver);
+			//File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+			//File DestFile=new File("C:\\Users\\ASUS\\Documents\\Automatizacion\\Capacitación\\leerarchivos");
+			//FileUtils.copyFile(SrcFile, DestFile);
 			driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
 			if (lblerror.isDisplayed()) {
 				System.out.println("ERROR!");
+				File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+				File DestFile=new File("C:\\Users\\ASUS\\Documents\\Automatizacion\\Capacitación\\leerarchivos\\"+System.currentTimeMillis()+".png");
+				FileUtils.copyFile(SrcFile, DestFile);
 			}
 			
 			
