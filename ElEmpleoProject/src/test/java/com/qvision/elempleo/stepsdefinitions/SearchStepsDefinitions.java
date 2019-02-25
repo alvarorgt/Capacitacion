fb92c2398d10876e89d860fa4f682373f48a2eb4
Alvaro <alvarorgt@gmail.com>
2019-02-24 23:08:35 -0500

    Inicio del ElEmpleo project...

---
+++ ElEmpleoProject/src/test/java/com/qvision/elempleo/stepsdefinitions/SearchStepsDefinitions.java
@@ -0,0 +1,28 @@
+package com.qvision.elempleo.stepsdefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class SearchStepsDefinitions {
+	
+	@Given("^I want to find jobs with a keyword$")
+	public void iWantToFindJobsWithAKeyword() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I search for jobs containing 'contador'$")
+	public void iSearchForJobsContainingContador() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I should only see items related to 'contador'$")
+	public void iShouldOnlySeeItemsRelatedToContador() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
