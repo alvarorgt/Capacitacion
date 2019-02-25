8c5e73474779a18152841ce4717c33fbb6f57603
AlvaroW <alvarorgt@gmail.com>
2019-02-25 10:53:30 -0500

    Modificado...

--- ElEmpleoProject/src/test/java/com/qvision/elempleo/stepsdefinitions/SearchStepsDefinitions.java
+++ ElEmpleoProject/src/test/java/com/qvision/elempleo/stepsdefinitions/SearchStepsDefinitions.java
@@ -1,28 +1,33 @@
 package com.qvision.elempleo.stepsdefinitions;
 
-import cucumber.api.PendingException;
+import static org.junit.Assert.assertTrue;
+
+import com.qvision.elemepleo.steps.InitialStep;
+
+//import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.thucydides.core.annotations.Steps;
 
 public class SearchStepsDefinitions {
 	
+	@Steps
+	InitialStep stepInitial;
+	
 	@Given("^I want to find jobs with a keyword$")
 	public void iWantToFindJobsWithAKeyword() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    stepInitial.openurl();
 	}
 
 	@When("^I search for jobs containing 'contador'$")
 	public void iSearchForJobsContainingContador() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    stepInitial.search("contador");
 	}
 
 	@Then("^I should only see items related to 'contador'$")
 	public void iShouldOnlySeeItemsRelatedToContador() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    assertTrue(stepInitial.validate());
 	}
 
 }
